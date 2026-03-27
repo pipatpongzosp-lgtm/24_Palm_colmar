8315f21860bdaaac3a128299f8644eb0bc5c866f
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-03-27 13:20:08 +0700

    add draft1 structure & header

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'custom': ['"DS_store"', 'sans-serif'], // เพิ่มฟอนต์ของคุณที่นี่
+      },
+    },
+  },
+}
